204eaee5b2122e37da3ab801a4bb625473d7a615
pushpa112 <pushpa112@users.noreply.github.com>
2021-05-17 23:38:05 -0500

    Ecomm-Database created inside SQL window

--- ECommDatabase/ECContext.cs
+++ ECommDatabase/ECContext.cs
@@ -22,10 +22,22 @@ namespace ECommDatabase
 
     }
 }
-/*
+/********************STEP1: *********************************
  * Go to Tools > NuGet Packet Manager > Packet Manager Console
  * enable-migrations: this code will generate Configuration file
  * add-migration initialized: This code will generate migration file(where tables, entities, keys etc are defined)
  * update-database: This code will add up the database inside Sql Server
  * Now, check the Server Explorer/ SQL Server Object Explorer to see if the database gets created there
+ */
+
+/********************************STEP2:***********************************
+ * Inside SQL Server Object Explorer window, ECommConnection will show up. EComm-Database should show up there.
+ * EcommWeb was setup as the startup project. That's the reason EcommConnection showed up there.
+ * Now, goto Packet Manager Console, write:
+ * enable-migrations
+ * enable-migrations -verbose
+ * Now, change the start-up project to ECommDatabase and then write
+ * enable-migrations -verbose -force
+ * update-database -verbose
+ * Now, EComm-Database will appear inside the SQL Server Object Explorer Window
  */
